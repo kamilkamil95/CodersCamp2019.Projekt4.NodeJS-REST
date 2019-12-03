06548612916f2be4b7833f38458cdab854a69c58
MateuszNowak <kontakt.mateusznowak@gmail.com>
2019-12-03 19:35:07 +0100

    #1 Add @types for dependencies

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,11 @@
+import express from 'express';
+
 console.log("Hello from TS!");
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send('Hello World');
+});
+
+app.listen(5000, () => console.log('Server running!'));
