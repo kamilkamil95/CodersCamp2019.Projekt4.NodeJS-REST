b91492c21ba6518679dc102509af9011f7d68ecb
MateuszNowak <kontakt.mateusznowak@gmail.com>
2019-12-04 23:28:58 +0100

    #1 Add configuration for db

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,3 @@
 import {ExpressServer} from "./sharedkernel/infrastructure/ExpressServer";
-import {isDefined} from "./utils";
 
-const port = isDefined(process.env.PORT) ? parseInt(process.env.PORT) : undefined;
-ExpressServer.start(port);
+ExpressServer.start();
